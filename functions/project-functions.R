c2161f02bf6ed555330b5bb6a21e3661b3bc6eae
attahj <jaleelattah@live.com>
2020-11-07 15:15:54 -0800

    Update project-functions.R

--- functions/project-functions.R
+++ functions/project-functions.R
@@ -1,8 +1,12 @@
-dataclean = function()
+getdata = function()
 {
   #open the file and parse into df
   path.to.secret = "C:/Users/blue/Desktop/STAT419/project/";
   measure_raw = utils::read.csv( paste0(path.to.secret, "cm.final.measure.txt"), header=TRUE, quote="", sep="|");
-  measure_raw = measure_raw[-c(1,8:12,14,15,16,17,18,28)]
   return(measure_raw)
 }
+prepareMeasureData = function(x)
+{
+  return(x[-c(1,8:12,14,15,16,17,18,28)])
+}
+
