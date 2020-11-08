e728338a5177f764aeaf7c31afedc5475dabf979
attahj <jaleelattah@live.com>
2020-11-08 12:15:50 -0800

    Update project-functions.R

--- functions/project-functions.R
+++ functions/project-functions.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 options(scipen=999)
+library(scatterplot3d)
 getdata = function()
 {
   #open the file and parse into df
@@ -11,6 +12,8 @@ prepareMeasureData = function(x)
 {
   measure = x[-c(1,8:12,14,15,16,17,18,28)]
   measure = measure[c(2,7:8,18)]
+  #convert gender to factor
+  measure$my.gender = as.factor(measure$my.gender)
   return(measure)
 }
 getmale = function(x)
@@ -29,4 +32,33 @@ cleanoutlier = function(x,y)
 {
   new = rbind(x,y) %>% filter(hand.length<50)
   return (new)
+}
+build3correlationtables = function(x,y,z)
+{
+  #all genders
+  buildLatexCorrelationTable(as.matrix(getnumeric(x)),myFile = "C:/_git_/WSU_STATS419_FALL2020/project-measure/tables/table-both-correlation.tex",myCaption = "Descriptive Statistics and Correlation Analysis for Both Genders",rotateTable=FALSE)
+  #males
+  buildLatexCorrelationTable(as.matrix(getnumeric(y)),myFile ="C:/_git_/WSU_STATS419_FALL2020/project-measure/tables/table-male-correlation.tex",myCaption = "Descriptive Statistics and Correlation Analysis for Males",rotateTable=FALSE)
+  #females
+  buildLatexCorrelationTable(as.matrix(getnumeric(z)),myFile ="C:/_git_/WSU_STATS419_FALL2020/project-measure/tables/table-female-correlation.tex",myCaption = "Descriptive Statistics and Correlation Analysis for Females",rotateTable=FALSE)
+}
+linearRegression = function(x)
+{
+  return(lm(height ~ ., getnumeric(x)))
+}
+dataRegressionPlot = function(x)
+{
+  colors = c("#E69F00", "#56B4E9")
+  colors = colors[as.numeric(x$my.gender)]
+  plot = scatterplot3d(x$hand.length,x$age,x$height, angle=50, pch = 16, color=adjustcolor(colors,alpha.f=0.6),  box=FALSE,main="Male and Female Regression Plot")
+  plot$plane3d((lm(height ~ ., getnumeric(x)))
+  legend("topright", legend = levels(x$my.gender), col = c("#E69F00", "#56B4E9"), pch = 16)
+}
+dataRegressionPlotSeperateGender = function(x,y,z)
+{
+  colors = c("#E69F00", "#56B4E9")
+  colors = colors[as.numeric(x$my.gender)]
+  plot = scatterplot3d(x$hand.length,x$age,x$height, angle=50, pch = 16, color=adjustcolor(colors,alpha.f=0.6),  box=FALSE,main=z)
+  plot$plane3d(y)
+  legend("topright", legend = levels(x$my.gender), col = c("#E69F00", "#56B4E9"), pch = 16)
 }
